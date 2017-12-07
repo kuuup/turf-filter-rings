dbc5a13a08c91e94ea7670e3632556eb0e7c050c
Peter Kupietz <p.kupietz@socoto.com>
2017-12-07 13:46:47 +0100

    Update transpiled file

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
-
 var _turfPolygon = require('turf-polygon');
 
 var _turfPolygon2 = _interopRequireDefault(_turfPolygon);
@@ -24,34 +22,25 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
  */
 var filterRings = function filterRings(feature, filterFn) {
     if (feature.geometry.type === 'Polygon') {
-        var _ret = function () {
+        var rings = [];
+        feature.geometry.coordinates.forEach(function (ring) {
+            if (filterFn(ring)) rings.push(ring);
+        });
+        if (rings.length === 0) throw new Error('All rings removed!');
+        return (0, _turfPolygon2.default)(rings, feature.properties || {});
+    } else if (feature.geometry.type === 'MultiPolygon') {
+        var outerRings = [];
+        feature.geometry.coordinates.forEach(function (outerRing) {
             var rings = [];
-            feature.geometry.coordinates.forEach(function (ring) {
+            outerRing.forEach(function (ring) {
                 if (filterFn(ring)) rings.push(ring);
             });
-            if (rings.length === 0) throw new Error('All rings removed!');
-            return {
-                v: (0, _turfPolygon2.default)(rings, feature.properties || {})
-            };
-        }();
-
-        if ((typeof _ret === 'undefined' ? 'undefined' : _typeof(_ret)) === "object") return _ret.v;
-    } else if (feature.geometry.type === 'MultiPolygon') {
-        var _ret2 = function () {
-            var outerRings = [];
-            feature.geometry.coordinates.forEach(function (outerRing) {
-                var rings = [];
-                outerRing.forEach(function (ring) {
-                    if (filterFn(ring)) rings.push(ring);
-                });
-                outerRings.push(rings);
-            });
-            return {
-                v: (0, _turfMultipolygon2.default)(outerRings, feature.properties || {})
-            };
-        }();
-
-        if ((typeof _ret2 === 'undefined' ? 'undefined' : _typeof(_ret2)) === "object") return _ret2.v;
+            if (rings.length > 0) outerRings.push(rings);
+        });
+        if (outerRings.length < 1) {
+            throw new Error('All rings removed!');
+        }
+        return (0, _turfMultipolygon2.default)(outerRings, feature.properties || {});
     } else {
         throw new Error('Input feature must be a Polygon or MultiPolygon!');
     }
